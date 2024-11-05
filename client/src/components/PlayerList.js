9e44098c57b09791f3d76d26f725bc7d9a14a731
ANANTHA PUNEETH CHOWDARY <96817637+puneeth1501@users.noreply.github.com>
2024-11-05 03:25:45 -0600

    Update PlayerList.js

--- client/src/components/PlayerList.js
+++ client/src/components/PlayerList.js
@@ -1,23 +1,4 @@
-// import React from 'react';
 
-// function PlayerList({ players, currentPlayer }) {
-//     return (
-//         <div className="player-list">
-//             <h3>Players</h3>
-//             {players.map(player => (
-//                 <div 
-//                     key={player.id}
-//                     className={`player-item ${player.id === currentPlayer ? 'active' : ''}`}
-//                 >
-//                     {player.name}
-//                     {player.id === currentPlayer && ' (Current Turn)'}
-//                 </div>
-//             ))}
-//         </div>
-//     );
-// }
-
-// export default PlayerList;
 import React from 'react';
 
 function PlayerList({ players, currentPlayer }) {
